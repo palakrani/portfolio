09b52f98ff7a7562bda77690c5552ae12680fa51
Palak Rani <palakirani018@gmail.com>
2023-03-14 23:33:21 +0530

    first commit

---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+     <BsLinkedin />
+    </div>
+    <div>
+      <BsGithub />
+    </div>
+  </div>
+);
+
+export default SocialMedia;
